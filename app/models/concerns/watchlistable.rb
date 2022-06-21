388ebad3fe5071537a3887762368a188f68da56d
mark mcdonald <mark.mcdonald@gmail.com>
2022-06-21 13:30:33 +0700

    new

--- app/models/concerns/watchlistable.rb
+++ app/models/concerns/watchlistable.rb
@@ -1,16 +1,19 @@
 module Watchlistable
     extend ActiveSupport::Concern
-    attr_reader :action_names
+    attr_reader :wachlist_action_names
   
     #included do
     #  puts "Called"
     #end  
     class_methods do
+
       def has_many_watchlists(action_names:, class_name:)
-        @action_names = action_names  
+        @wachlist_action_names = action_names  
+
         @action_names.each_with_index do |name, i|
           has_many :"#{name}_watch", -> {where(watchlist_type: i)}, class_name: 'Watchlist'
-          has_many :"#{name}_#{class_name.to_s.downcase.pluralize}", through: :"#{name}_watch", source: :watchable, source_type: "#{class_name}"
+          has_many :"#{name}_#{class_name.to_s.downcase.pluralize}",
+           through: :"#{name}_watch", source: :watchable, source_type: "#{class_name}"
         end
       end
     end
